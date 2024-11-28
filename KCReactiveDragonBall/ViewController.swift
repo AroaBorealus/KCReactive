bb4352c33e757a063b6afc5eb7c08217203add51
AroaMG Mac <aroaborealus@gmail.com>
2024-11-28 18:06:23 +0100

    Initial Commit

---
+++ KCReactiveDragonBall/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KCReactiveDragonBall
+//
+//  Created by Aroa Miguel Garcia on 28/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ KCReactiveDragonBallTests/KCReactiveDragonBallTests.swift
@@ -0,0 +1,17 @@
+//
+//  KCReactiveDragonBallTests.swift
+//  KCReactiveDragonBallTests
+//
+//  Created by Aroa Miguel Garcia on 28/11/24.
+//
+
+import Testing
+@testable import KCReactiveDragonBall
+
+struct KCReactiveDragonBallTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
